7f818b755fd291bf00ae7e6733b91c2ce8397db6
Ilyas Okutan <ilyasemin23@gmail.com>
2023-02-21 18:51:08 +0100

    day1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,10 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "./src/test/resources/features",//features folder path
-        glue = "stepdefinitions"//stepdefinitions path
+        glue = "stepdefinitions",//stepdefinitions path
+        tags = "@tesla"// "and" ikiside olmali "or" sadece biri olsa yeter "@tesla or @iphone"
 
 )
 public class Runner {
 }
 //Bu sinif Test caseleri RUN etmek icin kullanilir
 //Ve konfigurasyonlar icin kullanilir
+//Runner class, features file lar ile step defitions i birbirile baglaronfigurasyonlar icin kullanilir
